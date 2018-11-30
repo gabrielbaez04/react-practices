59a328b3b490beae5c8c279eb65809bce974cffa
Gabriel Baez <gabrielbaez04@gmail.com>
2018-11-30 16:38:27 -0400

    Adding modal

--- indecision_app/src/components/IndecisionApp.js
+++ indecision_app/src/components/IndecisionApp.js
@@ -3,10 +3,12 @@ import AddOption from './AddOption';
 import Options from './Options';
 import Action from './Action';
 import Header from './Header';
+import OptionModal from './OptionModal';
 
 class IndecisionApp extends React.Component {
     state = {
-        options: []
+        options: [],
+        selectedOption: undefined
       };
     componentDidMount() {
       try{
@@ -17,29 +19,28 @@ class IndecisionApp extends React.Component {
         }
       }catch(e){
         //Do nothing
-      }
-      
-    }
+      }     
+    };
     componentDidUpdate(prevProps, prevState) {
       if(prevState.options.length !== this.state.options.length){
         const json = JSON.stringify(this.state.options);
         localStorage.setItem('options',json);
       }
-    }
+    };
     
     handleDeleteOptions = () => {
       this.setState(() => ({options: []}));
-    }
+    };
     handleDeleteOption = option => {
       this.setState((prevState)=>({
         options: prevState.options.filter((opt) =>opt !== option)
       }))
-    }
+    };
     handlePick = () => {
       const randomNum = Math.floor(Math.random() * this.state.options.length);
       const option = this.state.options[randomNum];
-      alert(option);
-    }
+      this.setState({selectedOption : option})
+    };
     handleAddOption = option => {
       if (!option) {
         return 'Enter valid value to add item';
@@ -52,6 +53,9 @@ class IndecisionApp extends React.Component {
           options: prevState.options.concat(option)
         }
       ));
+    };
+    handleClearSelectedOption = () =>{
+        this.setState({selectedOption : undefined})
     }
     render() {
       const title = 'Indecision';
@@ -72,6 +76,9 @@ class IndecisionApp extends React.Component {
           <AddOption
             handleAddOption={this.handleAddOption}
           />
+          <OptionModal 
+            selectedOption={this.state.selectedOption}
+            handleClearSelectedOption={this.handleClearSelectedOption}/>
         </div>
       );
     }
