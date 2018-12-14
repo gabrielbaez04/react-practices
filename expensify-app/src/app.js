40b86f134c468e8b51a92893de8a748295afe627
gabrielbaez04 <gabrielbaez04@gmail.com>
2018-12-13 21:58:02 -0400

    Initial Commit

---
+++ expensify-app/src/app.js
@@ -0,0 +1,7 @@
+// stateless functional component
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css'
+import './styles/styles.scss'
+ReactDOM.render(<p>boilerplate</p>, document.getElementById('app'));
+  
